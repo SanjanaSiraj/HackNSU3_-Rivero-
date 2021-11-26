ed75db7bd47c24d4c1c31735fef397d88b8ff28f
SanjanaSiraj <sanjanasiraj412@gmail.com>
2021-11-26 12:42:06 +0600

    mid commit

---
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MapActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        //imageView=findViewById(R.id.)
+    }
+}
